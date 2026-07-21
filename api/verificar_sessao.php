2fcb432d10294afe0513cfb4bec14de9e62c74c1
ManosTech <304186228+Juvencio93@users.noreply.github.com>
2026-07-21 12:25:09 -0300

    Add JSON response for unauthenticated session

--- api/verificar_sessao.php
+++ api/verificar_sessao.php
@@ -1,11 +1,8 @@
 <?php
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
-    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-    exit;
-}
 session_start();
 if (!isset($_SESSION['usuario'])) {
     http_response_code(401);
+    header('Content-Type: application/json');
     echo json_encode(['erro' => 'Não autenticado']);
     exit;
 }
