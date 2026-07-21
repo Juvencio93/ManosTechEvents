074e9ca60d33ac79d003d1a2767f4c813ddd861d
ManosTech <304186228+Juvencio93@users.noreply.github.com>
2026-07-21 12:10:08 -0300

    Enforce HTTPS redirection in verificar_sessao.php
    Redirect to HTTPS if not already using it.

--- api/verificar_sessao.php
+++ api/verificar_sessao.php
@@ -1,4 +1,8 @@
 <?php
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
+    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    exit;
+}
 session_start();
 if (!isset($_SESSION['usuario'])) {
     http_response_code(401);
