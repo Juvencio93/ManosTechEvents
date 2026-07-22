e4b5c3357dd3856646d7309e9d27b8fa39fdc255
ManosTech <304186228+Juvencio93@users.noreply.github.com>
2026-07-22 15:03:37 -0300

    Refactor onload function syntax in previewLogoConfig

--- js/cliente.js
+++ js/cliente.js
@@ -48,7 +48,7 @@ function previewLogoConfig(event) {
     const file = event.target.files[0];
     if (file) {
         const reader = new FileReader();
-        reader.onload = function (e) {
+        reader.onload = function(e) {
             configLogoTemp = e.target.result;
             atualizarPreviewLogoConfig();
         };
