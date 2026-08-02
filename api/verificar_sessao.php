0232ad6c63f268aae5837991ecdbe838e23b162a
ManosTech <304186228+Juvencio93@users.noreply.github.com>
2026-08-02 00:11:48 -0300

    Delete api directory

--- api/verificar_sessao.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['usuario'])) {
-    http_response_code(401);
-    header('Content-Type: application/json');
-    echo json_encode(['erro' => 'Não autenticado']);
-    exit;
-}
